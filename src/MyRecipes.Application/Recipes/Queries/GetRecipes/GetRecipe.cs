93177963f141c740359df433c23e48181b68dd57
dmiky-xamk <dmiky001@edu.xamk.fi>
2022-07-18 13:51:35 +0300

    Add Query To Get Recipe
    Adds a query to get ingredients associated with the given ID.

--- src/MyRecipes.Application/Recipes/Queries/GetRecipes/GetRecipe.cs
+++ src/MyRecipes.Application/Recipes/Queries/GetRecipes/GetRecipe.cs
@@ -2,17 +2,18 @@
 using MediatR;
 using MyRecipes.Application.Common.Interfaces;
 using MyRecipes.Application.Common.Models;
+using MyRecipes.Application.Ingredients;
 
 namespace MyRecipes.Application.Recipes.Queries.GetRecipes;
 
 public class GetRecipe
 {
-    public class Query : IRequest<Result<RecipeDto>?>
+    public class Query : IRequest<Result<RecipeVm>?>
     {
         public int Id { get; set; }
     }
 
-    public class Handler : IRequestHandler<Query, Result<RecipeDto>?>
+    public class Handler : IRequestHandler<Query, Result<RecipeVm>?>
     {
         private readonly ICrud _db;
         private readonly IMapper _mapper;
@@ -23,17 +24,30 @@ public class GetRecipe
             _mapper = mapper;
         }
 
-        public async Task<Result<RecipeDto>?> Handle(Query request, CancellationToken cancellationToken)
+        public async Task<Result<RecipeVm>?> Handle(Query request, CancellationToken cancellationToken)
         {
             var recipe = await _db.GetRecipeAsync(request.Id);
             
             if (recipe is null)
             {
-                //return Result<RecipeDto>.Failure("No recipe was found with the given ID.");
                 return null;
             }
 
-            return Result<RecipeDto>.Success(_mapper.Map<RecipeDto>(recipe));
+            var ingredients = await _db.GetIngredientsAsync(recipe.Id);
+
+            if (!ingredients.Any())
+            {
+                // TODO: Log? Throw an exception? Should not happen.
+                return Result<RecipeVm>.Failure("Failed to find any ingredients associated with the recipe.");
+            }
+
+            RecipeVm fullRecipe = new()
+            {
+                Recipe = _mapper.Map<RecipeDto>(recipe),
+                Ingredients = _mapper.Map<List<IngredientDto>>(ingredients)
+            };
+
+            return Result<RecipeVm>.Success(fullRecipe);
         }
     }
 }

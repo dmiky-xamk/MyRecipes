4bcedfb9990343dc7ead7b147d45bce6fd897bc4
dmiky-xamk <dmiky001@edu.xamk.fi>
2022-07-30 08:25:27 +0300

    Create Validation Tests For RecipeDto

---
+++ tests/MyRecipes.Application.UnitTests/Recipes/Validation/RecipeDtoValidationTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentValidation.TestHelper;
+using MyRecipes.Application.Ingredients;
+using MyRecipes.Application.Recipes;
+
+namespace MyRecipes.Application.UnitTests.Recipes.Validation;
+
+public class RecipeDtoValidationTests
+{
+    private readonly RecipeDtoValidator _validator;
+
+    public RecipeDtoValidationTests()
+    {
+        _validator = new RecipeDtoValidator();
+    }
+
+    [Fact]
+    public void RecipeDto_ShouldHaveValidationErrorForEmptyRecipeName()
+    {
+        List<IngredientDto> ingredients = new()
+        {
+            new IngredientDto() { Name = "Flour", Unit = "dl", Amount = 4 }
+        };
+
+        var recipe = new RecipeDto
+        {
+            Name = string.Empty,
+            Ingredients = ingredients
+        };
+
+        var result = _validator.TestValidate(recipe);
+
+        result.ShouldHaveValidationErrorFor(recipe => recipe.Name);
+    }
+    
+    [Fact]
+    public void RecipeDto_ShouldHaveValidationErrorForEmptyIngredientName()
+    {
+        List<IngredientDto> ingredients = new()
+        {
+            new IngredientDto() { Name = string.Empty, Unit = "dl", Amount = 4 }
+        };
+
+        var recipe = new RecipeDto
+        {
+            Name = "Pancakes",
+            Ingredients = ingredients
+        };
+
+        var result = _validator.TestValidate(recipe);
+
+        result.ShouldHaveValidationErrorFor("Ingredients[0].Name");
+    }
+}

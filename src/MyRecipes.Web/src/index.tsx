5ec2e6be0741c7740759740f0277a24c662c8b0f
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-02-22 16:20:25 +0200

    Set up the index for the client application
    Defines some basic styling using MUI and CSS.
    Initializes queryClient for client-side caching.
    Initializes BrowserRouter for client-side routing.

--- src/MyRecipes.Web/src/index.tsx
+++ src/MyRecipes.Web/src/index.tsx
@@ -1,12 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createTheme } from "@mui/material/styles";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router-dom";
+import "./base.css";
+
+// Create a baseline MUI theme for the application.
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f7f7f7",
+    },
+    primary: {
+      main: "#ED6E06",
+    },
+  },
+  typography: {
+    fontFamily: ["Lato", "sans-serif"].join(","),
+  },
+});
+
+// Create a 'QueryClient' to set up default options for the caching.
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      useErrorBoundary: true,
+      refetchOnWindowFocus: false,
+    },
+  },
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <QueryClientProvider client={queryClient}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 );

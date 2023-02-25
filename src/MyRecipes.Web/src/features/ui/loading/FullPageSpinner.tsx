72ca4c0ccfe90e404fe4ca395cc5e37d26b6cc0a
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-02-25 12:59:19 +0200

    Refactor 'FullPageSpinner'
    Allows to pass a customized message to show while loading

--- src/MyRecipes.Web/src/features/ui/loading/FullPageSpinner.tsx
+++ src/MyRecipes.Web/src/features/ui/loading/FullPageSpinner.tsx
@@ -1,11 +1,11 @@
 import { CircularProgress, Stack, Typography } from "@mui/material";
 import PageContainer from "../main/PageContainer";
 
-export default function FullPageSpinner() {
+export default function FullPageSpinner({ text }: { text: string }) {
   return (
     <PageContainer center={true}>
       <Stack alignItems="center" justifyContent="center" gap={4}>
-        <Typography variant="h5">Opening the application...</Typography>
+        <Typography variant="h5">{text}</Typography>
         <CircularProgress />
       </Stack>
     </PageContainer>

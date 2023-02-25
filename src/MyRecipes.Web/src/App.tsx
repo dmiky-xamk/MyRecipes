7c16ee0dec4acdb5e6b42a116fed09773443535e
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-02-25 12:58:16 +0200

    Refactor loading screen. Delete default test.

--- src/MyRecipes.Web/src/App.tsx
+++ src/MyRecipes.Web/src/App.tsx
@@ -5,10 +5,26 @@ import FullPageSpinner from "./features/ui/loading/FullPageSpinner";
 import UnauthenticatedApp from "./UnauthenticatedApp";
 
 function App() {
+  // TODO: Get recipes and cache in 'Recipe' page (query)
+  // TODO: Cache recipes when calling 'useUser' HOW ??
+  // TODO: Bottom navigation with FAB
+  // TODO: 'Search' page with fuzzy autocomplete to search for cached recipes using ingredients as search parameters
+  // TODO: 'Shopping cart' page to mark which ingredients to buy (or complete recipes?)
+  // TODO: A way to log out
+  // TODO: Api returns recipe id as a string, remove toString() methods
+  // TODO: Ingredient index for adding and removing?
+  // TODO: Clear indication on the required / optional fields?
+
+  // TODO: Api id to string, is string in database.
+  // TODO: Amount to string in db.
+  // TODO: Have api return the updated recipe?
+
   return (
     <Fragment>
       <AuthLoader
-        renderLoading={() => <FullPageSpinner />}
+        renderLoading={() => (
+          <FullPageSpinner text="Opening the application..." />
+        )}
         renderUnauthenticated={() => <UnauthenticatedApp />}
       >
         <AuthenticatedApp />

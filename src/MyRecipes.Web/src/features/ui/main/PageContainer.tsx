1fc025ec3d22c1bd4f2fb00eb44f6a04b0f1f7ba
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-02-26 17:00:22 +0200

    Refactor 'PageContainer' to accept 'fullHeight' prop

--- src/MyRecipes.Web/src/features/ui/main/PageContainer.tsx
+++ src/MyRecipes.Web/src/features/ui/main/PageContainer.tsx
@@ -5,6 +5,7 @@ interface Props {
   children: ReactNode;
   maxWidth?: "xs" | "sm" | "md" | "lg" | "xl";
   center?: boolean;
+  fullHeight?: boolean;
   sx?: SxProps<Theme>;
 }
 
@@ -20,15 +21,17 @@ export default function PageContainer({
   children,
   maxWidth = "md",
   center = false,
+  fullHeight = false,
   sx,
 }: Props) {
   const centerContent = center === true ? centerSx : {};
+  const height = fullHeight ? "100%" : undefined;
 
   return (
     <Container
       component="main"
       maxWidth={maxWidth}
-      sx={{ mt: 4, ...sx, ...centerContent }}
+      sx={{ mt: 4, ...sx, ...centerContent, height }}
     >
       {children}
     </Container>

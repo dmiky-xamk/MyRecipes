95bc76bdd055ab5016563eac0612f980b65d7448
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-02-28 18:20:08 +0200

    Parse connection string from the hosting site's environment variables

--- src/MyRecipes.Application/Infrastructure/Persistence/PostgreSqlDataAccess.cs
+++ src/MyRecipes.Application/Infrastructure/Persistence/PostgreSqlDataAccess.cs
@@ -18,7 +18,7 @@ internal class PostgreSqlDataAccess : IDataAccess
     public PostgreSqlDataAccess(IConfiguration config)
     {
         _config = config;
-        _connectionString = _config.GetConnectionString("Postgre");
+        _connectionString = ParseConnectionString() ?? _config.GetConnectionString("Postgre");
     }
 
     /// <summary>
@@ -90,5 +90,28 @@ internal class PostgreSqlDataAccess : IDataAccess
         {
             return await connection.ExecuteAsync(sqlStatement, parameters);
         }
+    }
+
+    private static string? ParseConnectionString()
+    {
+        var connUrl = Environment.GetEnvironmentVariable("DATABASE_URL");
+
+        if (connUrl is null)
+        {
+            return null;
+        }
+
+        // Parse connection URL to connection string for Npgsql
+        connUrl = connUrl.Replace("postgres://", string.Empty);
+        var pgUserPass = connUrl.Split("@")[0];
+        var pgHostPortDb = connUrl.Split("@")[1];
+        var pgHostPort = pgHostPortDb.Split("/")[0];
+        var pgDb = pgHostPortDb.Split("/")[1];
+        var pgUser = pgUserPass.Split(":")[0];
+        var pgPass = pgUserPass.Split(":")[1];
+        var pgHost = pgHostPort.Split(":")[0];
+        var pgPort = pgHostPort.Split(":")[1];
+
+        return $"Server={pgHost};Port={pgPort};User Id={pgUser};Password={pgPass};Database={pgDb}; SSL Mode=Require; Trust Server Certificate=true";
     }
 }

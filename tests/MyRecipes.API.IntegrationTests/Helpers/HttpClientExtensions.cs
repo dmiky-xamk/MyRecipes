6ce4d93f7a1f4559fbbfead16369c676ed8caee2
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-03-28 12:12:24 +0300

    Add helpers to reduce boilerplate

---
+++ tests/MyRecipes.API.IntegrationTests/Helpers/HttpClientExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Net.Http.Headers;
+
+namespace MyRecipes.API.IntegrationTests.Helpers;
+
+public static class HttpClientExtensions
+{
+    public static void AddAuthorizationHeader(this HttpClient client, string token)
+    {
+        client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+    }
+}
---
+++ tests/MyRecipes.API.IntegrationTests/Helpers/Routes.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRecipes.API.IntegrationTests.Helpers;
+
+public static class Routes
+{
+    public static class V1
+    {
+        public const string Recipes = "/api/v1/recipes";
+        public const string Account = "/api/v1/account";
+        public const string AccountLogin = "/api/v1/account/login";
+        public const string AccountRegister = "/api/v1/account/register";
+    }
+}

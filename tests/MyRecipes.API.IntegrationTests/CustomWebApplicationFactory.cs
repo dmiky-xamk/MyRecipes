7ea98335b0e345cbc4f3d1fb0b5ed30f7d02b94d
dmiky-xamk <dmiky001@edu.xamk.fi>
2023-03-29 11:29:04 +0300

    Override token validation options for tests
    Overrides the options so that the tests can run properly in a pipeline

--- tests/MyRecipes.API.IntegrationTests/CustomWebApplicationFactory.cs
+++ tests/MyRecipes.API.IntegrationTests/CustomWebApplicationFactory.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using MyRecipes.Infrastructure.Persistence;
 using Npgsql;
@@ -23,6 +24,15 @@ public sealed class CustomWebApplicationFactory
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", "Development");
+
+        builder.ConfigureAppConfiguration(configurationBuilder =>
+        {
+            var integrationConfig = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            configurationBuilder.AddConfiguration(integrationConfig);
+        });
 
         builder.ConfigureTestServices(services =>
         {
